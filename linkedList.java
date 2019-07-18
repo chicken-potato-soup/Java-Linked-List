c6e0bc58ebaf8c916725de2d50f161712d6137ce
isaacdejesus <isaacderxc@gmail.com>
2019-07-17 22:34:00 -0500

    deleted unnecessary files

--- linkedList.java
+++ linkedList.java
@@ -72,4 +72,40 @@ class linkedList<DataType>
                 count++;
             }
     }
+    public void addAtIndex(DataType val, int index)
+    {
+        Node <DataType> current=head;
+        Node <DataType> newNode= new Node <DataType>();
+        newNode.info=val;
+        newNode.link=null;
+        //if the list is empty, new node becomes the first node
+        if(isEmpty())
+            {
+                head=newNode;
+                tail=newNode;
+                count++;
+            }
+        //check if index is greater than number of nodes on the list
+        else if(index>count)
+            {
+                //index is out of range, we add to the end of the list
+                addAtEnd(val);
+            }
+            //if index is <2 we add to the beginning
+        else if(index<2)
+            {
+                //add to the head
+                addBeginning(val);
+            }
+        else
+            {
+                for(int i=1; i<index-1; i++)
+                    {
+                        current=current.link;
+                    }
+                newNode.link=current.link;
+                current.link=newNode;
+                count++;
+            }
+    }
 }
--- test.java
+++ test.java
@@ -7,7 +7,10 @@ public class test
             linkedList<Integer> potato= new linkedList<Integer>();
             potato.addBeginning(9);
             potato.addBeginning(2);
+            potato.addAtIndex(15, 5);
+            potato.addAtIndex(11, 0);
+            potato.addAtIndex(7, 3);
+            potato.addAtIndex(99, 5);
             potato.display();
-
         }
 }
